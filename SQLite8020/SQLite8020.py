2429378d7b8dfde1e7204e3ca461bb42181dc089
Randall Nagy <randall.nagy@gmail.com>
2020-05-06 11:17:23 -0400

    Update SQLite8020.py

--- SQLite8020/SQLite8020.py
+++ SQLite8020/SQLite8020.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
     assert(dao.insert({"Name":"John2's", "String":"123, '45'", "Age":21}))
     assert(dao.insert({"Name":'''John3''', "Age":21}))
     assert(dao.update(1, {"Age":22}))
-    assert(dao.update(1, {"Name":"Jasper"}))
+    assert(dao.update(1, {"Name":"Jasper", "String":'888, "33"',}))
+    assert(dao.update(3, {"Name":"Pookie", "String":"654, '12'",}))
     assert(dao.insert({"Name":"Nagy", "Age":432}))
     assert(dao.count() == 4)
     for row in dao.select(f"select * from {dao.table_name};"):
